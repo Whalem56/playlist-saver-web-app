c142248db476118cf0d68a0d98d4272e1e95191d
Will Richardson <willrichardson56@gmail.com>
2019-07-10 17:10:51 -0500

    OAuth2 completed. Working on processing data. Learning promises.

--- app.js
+++ app.js
@@ -17,10 +17,19 @@ const redirect_uri = 'http://' + ip + ':' + portNumber + '/authenticate';
 const cookieStateKey = 'spotify_auth_state';
 const cookieStateLength = 16;
 
-let access_token;
-let refresh_token;
+let access_token = null;
+let refresh_token = null;
+
+let idToPlaylistMap = [];
+let numPlaylists = -1;
+
+let output = '';
 
 let app = express();
+
+app.use(express.static(__dirname + '/public'))
+  .use(cors())
+  .use(cookieParser());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -45,34 +54,103 @@ app.get('/login', (req, res) => {
 // Check state parameter on req. Respond by asking for refresh and access tokens
 app.get('/authenticate', (req, res) => {
   const code = req.query.code || null;
-  //const state = req.query.state || null;
-  //const storedState = req.cookies ? req.cookies[stateKey] : null;
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[cookieStateKey] : null;
+  console.log('state: ', state);
+  console.log('storedState: ', storedState);
+  console.log('req.cookies: ', req.cookies);
 
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-    },
-    json: true
-  }
-  request.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      access_token = body.access_token;
-      refresh_token = body.refresh_token;
+  if (state !== null && state === storedState) {
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        grant_type: 'authorization_code',
+        redirect_uri: redirect_uri,
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
     }
-    console.log(error);
-    console.log(response.statusCode);
-    //console.log(response);
-    //console.log(body);
-    console.log(access_token);
-    console.log(refresh_token);
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        access_token = body.access_token;
+        refresh_token = body.refresh_token;
+        console.log("access_token: ", access_token);
+        console.log("refresh_token: ", refresh_token);
+        res.redirect('/getPlaylists');
+      }
+    });
+  } else {
+    console.log('State mismatch');
     res.redirect('/');
-  });
+  }
+
 });
+
+
+app.get('/getPlaylists', async (req, res) => {
+  console.log("insid getPlaylists endpoint!!!!!!");
+  let options = {
+    url: 'https://api.spotify.com/v1/me/playlists',
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    qs: {
+      offset: 0,
+      limit: 50
+    },
+    json: true,
+  }
+  console.log('about to make request');
+
+  await request.get(options, async (error, response, body) => {
+    console.log('response.statusCode', response.statusCode);
+    proccessPlaylistData(body.items);
+
+    options.qs.offset += options.qs.limit;
+    numPlaylists = body.total;
+
+    while (options.qs.offset < numPlaylists ) {
+
+     await request.get(options, (error, response, body) => {
+        proccessPlaylistData(body.items);        
+        // console.log('============idToPlaylistMap============');
+         console.log(idToPlaylistMap);
+        // console.log('length of map', idToPlaylistMap.length);
+      });
+
+      options.qs.offset += options.qs.limit;
+    }
+  });
+  res.redirect('/getTracks');
+});
+
+
+app.get('/getTracks', (req, res) => {
+  console.log("inside getTracks endpoint!!!!!!");
+  console.log('numPlaylists', numPlaylists);
+  console.log('idToPlaylistMap.length', idToPlaylistMap.length);
+  // Wait until done querying for playslists
+  while(numPlaylists != idToPlaylistMap.length) {}
+  console.log(idToPlaylistMap);
+  // request.get(options, (error, response, body) {
+
+  // }
+  // res.download(filePathAndName, clientFileName);
+  res.redirect('/');
+
+});
+
+const proccessPlaylistData = (items) => {
+    const currMap = items.map((playlist) => {
+      let pair = {};
+      pair.id = playlist.id;
+      pair.name = playlist.name;
+      return pair;
+    });
+
+    idToPlaylistMap = idToPlaylistMap.concat(currMap);
+}
 
 // Generate random string of numbers and letters. Use this to for state.
 const generateRandomString = (length) => {
@@ -84,9 +162,5 @@ const generateRandomString = (length) => {
 
   return result;
 }
-
-app.use(express.static(__dirname + '/public'))
-  .use(cors())
-  .use(cookieParser());
 
 app.listen(portNumber, () => console.log(`App is listening on port: ${portNumber}`));
