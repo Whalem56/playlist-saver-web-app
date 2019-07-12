b277cf22efdc4b84178f851288de0a437b3b4a4d
Will Richardson <willrichardson56@gmail.com>
2019-07-11 19:33:29 -0500

    Added getting data. Need to refactor async await

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// fields=tracks.items.track(name,artists)
-
+const fs = require('fs');
 const express = require('express');
 const request = require('request');
+const rp = require('request-promise')
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const querystring = require('querystring');
@@ -21,9 +21,7 @@ let access_token = null;
 let refresh_token = null;
 
 let idToPlaylistMap = [];
-let numPlaylists = -1;
-
-let output = '';
+let listOfPlayToTracks = {};
 
 let app = express();
 
@@ -90,66 +88,168 @@ app.get('/authenticate', (req, res) => {
 });
 
 
-app.get('/getPlaylists', async (req, res) => {
-  console.log("insid getPlaylists endpoint!!!!!!");
+await app.get('/getPlaylists', async (req, res) => {
+
+  // Get playlists. Store in global: idToPlaylistMap
+  await getPlaylists();
+
+  // Get tracks associated with each playlist and stores in 
+  // global: playlistTracksMapList. Reads from global : idToPlaylistMap
+  await getTracks();
+
+  console.log('listOfPlayToTracks', listOfPlayToTracks);
+
+  let output = getOutput();
+
+  await fs.writeFile(__dirname +'/playlist.txt', output, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('The file was saved!');
+    }
+  });
+
+  res.redirect('/');
+});
+
+
+const getPlaylists = async () => {
+
   let options = {
     url: 'https://api.spotify.com/v1/me/playlists',
-    headers: { 'Authorization': 'Bearer ' + access_token },
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    },
     qs: {
       offset: 0,
       limit: 50
     },
     json: true,
   }
-  console.log('about to make request');
 
-  await request.get(options, async (error, response, body) => {
-    console.log('response.statusCode', response.statusCode);
+  let numPlaylists = -1;
+
+  // Make first request for playlists. 
+  await rp.get(options, async (error, response, body) => {
+    //console.log('response.statusCode', response.statusCode);
     proccessPlaylistData(body.items);
 
-    options.qs.offset += options.qs.limit;
     numPlaylists = body.total;
+  });
 
-    while (options.qs.offset < numPlaylists ) {
+  options.qs.offset += options.qs.limit;
 
-     await request.get(options, (error, response, body) => {
-        proccessPlaylistData(body.items);        
-        // console.log('============idToPlaylistMap============');
-         console.log(idToPlaylistMap);
-        // console.log('length of map', idToPlaylistMap.length);
+  // Continue making requests requests for playlists if limit is less than 
+  // the amount of playlists
+  while (options.qs.offset < numPlaylists) {
+    await rp.get(options, async (error, response, body) => {
+      proccessPlaylistData(body.items);
+    });
+
+    options.qs.offset += options.qs.limit;
+  }
+}
+
+const proccessPlaylistData = (items) => {
+
+  const currMap = items.map((playlist) => {
+    let pair = {};
+    pair.id = playlist.id;
+    pair.name = playlist.name;
+    return pair;
+  });
+
+  idToPlaylistMap = idToPlaylistMap.concat(currMap);
+}
+
+const getTracks = async () => {
+  console.log('inside getTracks()');
+  let options = {
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    },
+    qs: {
+      fields: 'total,items(track(name,artists))',
+      offset: 0,
+      limit: 100
+    },
+    json: true,
+  }
+
+  await idToPlaylistMap.forEach(async (pair) => {
+    const playlistId = pair.id;
+    const playlistName = pair.name;
+    let numTracks = 0;
+    options.offset = 0;
+    options.url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+    
+    await rp.get(options, async (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      processTrackData(playlistName, body.items);
+      numTracks = body.total;
+    });
+
+    console.log('numTracks: ', numTracks);
+
+    options.qs.offset += options.qs.limit;
+
+    // Continue making requests for tracks if limit is less than 
+    // the amount of playlists
+    while (options.qs.offset < numTracks) {
+      await rp.get(options, async (error, response, body) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log(response.statusCode);
+        processTrackData(playlistName, body.items);
       });
 
       options.qs.offset += options.qs.limit;
     }
   });
-  res.redirect('/getTracks');
-});
 
+  console.log('Inside getTracks(). listOfPlayToTracks: \n', listOfPlayToTracks);
 
-app.get('/getTracks', (req, res) => {
-  console.log("inside getTracks endpoint!!!!!!");
-  console.log('numPlaylists', numPlaylists);
-  console.log('idToPlaylistMap.length', idToPlaylistMap.length);
-  // Wait until done querying for playslists
-  while(numPlaylists != idToPlaylistMap.length) {}
-  console.log(idToPlaylistMap);
-  // request.get(options, (error, response, body) {
+  // TESTING ONLY
+  // options.url = `https://api.spotify.com/v1/playlists/6JTPtzzATO79FeNCfdz8rV/tracks`;
+  // await rp.get(options, async (error, response, body) => {
+  //   const playlistName = 'Sublime Playlist';
+  //   processTrackData(playlistName, body.items);
+  // });
+}
 
-  // }
-  // res.download(filePathAndName, clientFileName);
-  res.redirect('/');
+const processTrackData = (playlistName, items) => {
+  
+  const parsedTracks = items.map((item) => {
+    return {
+      'trackName': item.track.name,
+      'trackArtist': item.track.artists[0].name
+    };
+  });
 
-});
+  if (listOfPlayToTracks.hasOwnProperty(playlistName)) {
+    listOfPlayToTracks[playlistName] = listOfPlayToTracks[playlistName].concat(parsedTracks);
+  } else {
+    listOfPlayToTracks[playlistName] = parsedTracks;
+  }
+}
 
-const proccessPlaylistData = (items) => {
-    const currMap = items.map((playlist) => {
-      let pair = {};
-      pair.id = playlist.id;
-      pair.name = playlist.name;
-      return pair;
-    });
+const getOutput = () => {
+  let output = '';
+  console.log('listOfPlayToTracks: \n', listOfPlayToTracks);
+  for (let playlist in listOfPlayToTracks) {
+    if (listOfPlayToTracks.hasOwnProperty(playlist)) {
+      output += `${playlist}:\n\n`;
+      console.log(listOfPlayToTracks[playlist].length);
+      listOfPlayToTracks[playlist].forEach((track) => {
+        output += `\t${track.trackName}  -  ${track.trackArtist}\n`;
+      });
+    }
+  }
 
-    idToPlaylistMap = idToPlaylistMap.concat(currMap);
+  return output;
 }
 
 // Generate random string of numbers and letters. Use this to for state.
